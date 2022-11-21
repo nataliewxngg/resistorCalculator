ca7d167d8fb37e0cbc7a5e82878cdc6b208af230
natalie <wong.lt.natalie@gmail.com>
2022-11-21 12:12:27 -0500

    Edited hover effects of buttons on the menu

--- main.py
+++ main.py
@@ -11,26 +11,51 @@ running=True
 white=(255,255,255)
 
 # menu
-def menu():
-    win.fill((214,206,195))
-    pygame.display.set_icon(pygame.image.load("assets/icon.png"))
+def menuTitleText():
+    titleFont=pygame.font.Font("assets/GOUDOSB.TTF",60)
 
-    font=pygame.font.Font("assets/GOUDOSB.TTF",60)
-
-    resistorText=font.render("RESISTOR",True,white)
+    resistorText=titleFont.render("RESISTOR",True,white)
     resistorTextRect=resistorText.get_rect()
     resistorTextRect.left=300
     resistorTextRect.top=50
     win.blit(resistorText,resistorTextRect)
 
-    calculatorText=font.render("CALCULATOR",True,white)
+    calculatorText=titleFont.render("CALCULATOR",True,white)
     calculatorTextRect=calculatorText.get_rect()
     calculatorTextRect.left=340
     calculatorTextRect.top=100
     win.blit(calculatorText,calculatorTextRect)
 
+def menuButtons():
+    mouse=pygame.mouse.get_pos()
+    thirdColor=white
+    fourthColor=white
+    fifthColor=white
+
+    if mouse[0]>420 and mouse[0]<520 and mouse[1]>200 and mouse[1]<300:
+        thirdColor=(0,0,0)
+
+    elif mouse[0]>530 and mouse[0]<630 and mouse[1]>200 and mouse[1]<300:
+        fourthColor=(0,0,0)
+    
+    elif mouse[0]>640 and mouse[0]<740 and mouse[1]>200 and mouse[1]<300:
+        fifthColor=(0,0,0)
+
+    buttonThree=pygame.draw.rect(win,thirdColor,[420,200,100,100])
+    buttonFour=pygame.draw.rect(win,fourthColor,[530,200,100,100]) 
+    buttonFive=pygame.draw.rect(win,fifthColor,[640,200,100,100])
+
+def menu():
+    mouse=pygame.mouse.get_pos()
+    win.fill((214,206,195))
+    pygame.display.set_icon(pygame.image.load("assets/icon.png"))
+
+    menuTitleText()
+
     snowmanImg=pygame.image.load("assets/snowman.png")
-    win.blit(snowmanImg,(40,0))
+    win.blit(snowmanImg,(30,0))
+
+    menuButtons()
 
 while running:
     for event in pygame.event.get():
